927686f5d63b949f452b247b49816c4446b2854d
xtz <xutaozhe@126.com>
2023-04-15 23:51:03 +0800

    enabled cusdis

--- blog.config.js
+++ blog.config.js
@@ -51,7 +51,7 @@ const BLOG = {
       repo: ''
     },
     cusdisConfig: {
-      appId: '', // data-app-id
+      appId: '1fc55bfb-39f3-44ae-aef0-d619473f9619', // data-app-id
       host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
       scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
     }
